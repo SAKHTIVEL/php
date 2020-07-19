ff32dc8d1753f64db6b6e6418d03f7f073e9e714
SAKHTIVEL <msakthispear@gmail.com>
2020-07-19 08:37:10 +0530

    File upload1 completed

--- upload.php
+++ upload.php
@@ -1,5 +1,9 @@
 <?php 
 
-echo $_FILES;
-
+$name= $_FILES['myfile']['name'];
+$type = $_FILES['myfile']['type'];
+$size = $_FILES['myfile']['size'];
+$temp = $_FILES['myfile']['tmp_name'];
+$error = $_FILES['myfile']['error'];
+echo $error;
 ?>
